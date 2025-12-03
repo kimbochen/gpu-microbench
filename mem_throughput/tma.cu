bc4f61dc234778e48672a880ebfae78e49d5c5b6
Kimbo Chen <chentenghung@gmail.com>
2025-12-03 19:58:10 +0000

    Save files.

--- mem_throughput/tma.cu
+++ mem_throughput/tma.cu
@@ -1,43 +1,52 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cooperative_groups.h>
 #include <cuda_runtime.h>
-#include <cuda_pipeline.h>
 #include <cuda/barrier>
 #include <cuda/ptx>
 #include "utils.h"
 
 using barrier = cuda::barrier<cuda::thread_scope_block>;
+namespace ptx = cuda::ptx;
 
-constexpr int32_t NUM_SMS = 148;                            // B200 has 148 SMs
-constexpr int32_t L2_SIZE = 132644864;                      // B200 L2 cache is 126.5 MiB
-constexpr size_t MAX_LOAD_SIZE = 2LL * 1024 * 1024 * 1024;  // 2 GB
+constexpr size_t LOAD_SIZE = 1024;
+constexpr size_t ELEMS_PER_LOAD = LOAD_SIZE / sizeof(float);
+constexpr size_t NUM_LOADS = 4;
 
 
-__global__ void BulkAsyncCopyKernel(float *arr, size_t load_size, size_t N) {
-    __shared__ alignas(16) float buff[32768 / sizeof(float)];  // 32 KiB of shared memory
+__global__ void BulkAsyncCopyKernel(float *arr, size_t N) {
+    __shared__ alignas(16) float buff[(LOAD_SIZE * NUM_LOADS) / sizeof(float)];
 
     #pragma nv_diag_suppress static_var_with_dynamic_init
-    __shared__ barrier bar;
+    __shared__ barrier bar[NUM_LOADS];
 
     if (threadIdx.x == 0) {
-        init(&bar, blockDim.x);
+        for (int32_t i = 0; i < NUM_LOADS; i++) {
+            init(&bar[i], 1);
+        }
     }
     __syncthreads();
 
-    size_t offset = blockIdx.x * (load_size / sizeof(float));
-    size_t stride = gridDim.x * (load_size / sizeof(float));
+    size_t offset = blockIdx.x;
+    size_t stride = gridDim.x;
 
-    for (size_t i = offset; i < N; i += stride) {
+    for (size_t i = offset, j = 0; i < N / ELEMS_PER_LOAD; i += stride, j++) {
         if (threadIdx.x == 0) {
-            cuda::memcpy_async(buff, arr + i, cuda::aligned_size_t<16>(load_size), bar);
+            cg::invoke_one(cg::coalesced_threads(), [&] {
+                float *smem_ptr = buff + ELEMS_PER_LOAD * j;
+                float *gmem_ptr = arr + ELEMS_PER_LOAD * i;
+
+                ptx::cp_async_bulk(ptx::space_shared, ptx::space_global, smem_ptr, gmem_ptr, LOAD_SIZE, &bar[j % NUM_LOADS]);
+                ptx::mbarrier_arrive_expect_tx(ptx::sem_release, ptx::scope_cta, ptx::space_shared, &bar[j % NUM_LOADS], LOAD_SIZE);
+            });
         }
-        barrier::arrival_token token = bar.arrive();
-        bar.wait(std::move(token));
+
+        while (!ptx::mbarrier_try_wait_parity(&bar[j % NUM_LOADS], 0));
     }
 }
 
 
-void benchBulkAsyncCopyThroughput(int32_t num_blks_factor, size_t load_size) {
+void benchBulkAsyncCopyThroughput(int32_t num_blks_factor) {
     void *flush_arr;
     CHECK_CUDA(cudaMalloc(&flush_arr, L2_SIZE));
     CHECK_CUDA(cudaMemset(flush_arr, 0xA5, L2_SIZE));
@@ -46,7 +55,7 @@ void benchBulkAsyncCopyThroughput(int32_t num_blks_factor, size_t load_size) {
 
     float *arr, *d_arr;
     int32_t num_blks = NUM_SMS * num_blks_factor;
-    size_t arr_size = MIN_MULTIPLE(MAX_LOAD_SIZE, (num_blks * load_size));
+    size_t arr_size = MIN_MULTIPLE(MAX_DATA_VOLUME, (num_blks * LOAD_SIZE));
     size_t N = arr_size / sizeof(float);
 
     arr = (float*) malloc(arr_size);
@@ -63,7 +72,7 @@ void benchBulkAsyncCopyThroughput(int32_t num_blks_factor, size_t load_size) {
     cudaEventCreate(&stop);
 
     cudaEventRecord(start);
-    BulkAsyncCopyKernel<<<num_blks, 1>>>(d_arr, load_size, N);
+    BulkAsyncCopyKernel<<<num_blks, 1>>>(d_arr, N);
     CHECK_CUDA(cudaDeviceSynchronize());
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
@@ -71,7 +80,7 @@ void benchBulkAsyncCopyThroughput(int32_t num_blks_factor, size_t load_size) {
 
     float t_elapsed;
     cudaEventElapsedTime(&t_elapsed, start, stop);
-    printf("%d, %lu, %lu, %.5f\n", num_blks_factor, load_size, arr_size, t_elapsed);
+    printf("%d, %d, %lu, %.5f\n", num_blks_factor, LOAD_SIZE, arr_size, t_elapsed);
 
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
@@ -81,13 +90,13 @@ void benchBulkAsyncCopyThroughput(int32_t num_blks_factor, size_t load_size) {
 
 
 int main(int argc, char **argv) {
-    if (argc != 4) {
-        puts("Usage: ./tma [DEVICE_ID] [NUM_BLKS_FACTOR] [LOAD_SIZE]");
+    if (argc != 3) {
+        puts("Usage: ./tma [DEVICE_ID] [NUM_BLKS_FACTOR]");
         return 1;
     }
 
     cudaSetDevice(atoi(argv[1]));
-    benchBulkAsyncCopyThroughput(atoi(argv[2]), atoi(argv[3]));
+    benchBulkAsyncCopyThroughput(atoi(argv[2]));
 
     return 0;
 }
